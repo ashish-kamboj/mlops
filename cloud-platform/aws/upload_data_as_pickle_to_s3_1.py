59c9d09b45f712ef059aeb33b4ccc6a2af0f542b
Ashish Kamboj <ashu.009kamboj@gmail.com>
2023-01-18 19:03:13 +0530

    Code for putting data as a pickle file in s3 using Object.put()

---
+++ cloud-platform/aws/upload_data_as_pickle_to_s3_1.py
@@ -0,0 +1,33 @@
+## Importing Libraries
+import boto3
+import pickle
+
+## Data to upload as pickle file
+data = #<< Data can be a dataframe, dictionary, text data etc. (In general any data)>>
+
+## Constants
+AWS_ACCESS_KEY_ID = '<< >>'
+AWS_SECRET_ACCESS_KEY = '<< >>'
+
+S3_BUCKET_NAME='<< >>'
+S3_PREFIX = '<< >>'
+FILE_NAME = 'pickle_model.pkl'
+
+if(S3_PREFIX is not None or S3_PREFIX.strip() != ''):
+    key = S3_PREFIX + '/' + FILE_NAME
+else:
+    key = FILE_NAME
+
+## Creating s3 resource object
+s3_resource = boto3.resource(
+                's3',
+                region_name='us-east-1',
+                aws_access_key_id=AWS_ACCESS_KEY_ID,
+                aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+)
+
+## Creating Pickle object
+pickle_byte_obj = pickle.dumps(data)
+
+## Putting file to s3
+s3_resource.Object(S3_BUCKET_NAME,key).put(Body=pickle_byte_obj)
