86672e05243a75f83ee49bd38bb40f6baa78872c
Ashish Kamboj <ashu.009kamboj@gmail.com>
2023-03-05 18:20:17 +0530

    Code for getting data from dynamodb table using boto3

---
+++ cloud-platform/aws/get_data_from_dynamodb_table.py
@@ -0,0 +1,27 @@
+## Importing Libraries
+import boto3
+
+## AWS Key's
+AWS_ACCESS_KEY_ID = 'access_key_id'
+AWS_SECRET_ACCESS_KEY = 'secret_access_key'
+
+## Define DynamoDB table name
+table_name = 'table_name'
+
+## Creating s3 resource object
+dynamodb_client= boto3.client(
+                'dynamodb',
+                region_name='us-east-1',
+                aws_access_key_id=AWS_ACCESS_KEY_ID,
+                aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+)
+
+## Get DynamoDB table
+table = dynamodb_client.Table(table_name)
+
+## Get all items from DynamoDB table
+response = table.scan()
+
+## Print all items
+for item in response['Items']:
+    print(item)
