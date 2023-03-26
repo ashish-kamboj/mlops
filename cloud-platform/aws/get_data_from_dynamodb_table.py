d6c1ea08ffae1e485b6090bcdcec6ea6ba10e10c
Ashish Kamboj <ashu.009kamboj@gmail.com>
2023-03-26 18:32:43 +0530

    Updated dynamodb resource object name

--- cloud-platform/aws/get_data_from_dynamodb_table.py
+++ cloud-platform/aws/get_data_from_dynamodb_table.py
@@ -8,16 +8,15 @@ AWS_SECRET_ACCESS_KEY = 'secret_access_key'
 ## Define DynamoDB table name
 table_name = 'table_name'
 
-## Creating s3 resource object
-dynamodb_client= boto3.resource(
-                'dynamodb',
-                region_name='us-east-1',
-                aws_access_key_id=AWS_ACCESS_KEY_ID,
-                aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+## Creating dynamodb resource object
+dynamodb = boto3.resource('dynamodb',
+                           region_name='us-east-1',
+                           aws_access_key_id=AWS_ACCESS_KEY_ID,
+                           aws_secret_access_key=AWS_SECRET_ACCESS_KEY
 )
 
 ## Get DynamoDB table
-table = dynamodb_client.Table(table_name)
+table = dynamodb.Table(table_name)
 
 ## Get all items from DynamoDB table
 response = table.scan()
