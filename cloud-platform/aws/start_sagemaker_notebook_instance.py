07e719788dd6c7afbea1c7241a7ed1507adcd1ca
Ashish Kamboj <ashu.009kamboj@gmail.com>
2023-04-26 22:02:41 +0530

    Code for starting AWS Sagemaker notebook instance using boto3

---
+++ cloud-platform/aws/start_sagemaker_notebook_instance.py
@@ -0,0 +1,24 @@
+## Importing Libraries
+import boto3
+
+## Constants
+AWS_ACCESS_KEY_ID = 'access_key_id'
+AWS_SECRET_ACCESS_KEY = 'secret_access_key'
+
+region_name = 'us-east-1'
+
+## Set up the Amazon SageMaker client
+sagemaker = boto3.client('sagemaker',
+                          region_name=region_name, 
+                          aws_access_key_id=AWS_ACCESS_KEY_ID,
+                          aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+## Define the name of the notebook instance to start
+notebook_instance_name = 'notebook-instance-name'
+
+## Start the notebook instance
+sagemaker.start_notebook_instance(
+    NotebookInstanceName=notebook_instance_name
+)
+
+print(f"Notebook instance '{notebook_instance_name}' started successfully.")
