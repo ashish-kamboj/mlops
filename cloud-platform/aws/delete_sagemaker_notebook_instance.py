90c529263d58d0317f6be5a39ee8f29215eb0d56
Ashish Kamboj <ashu.009kamboj@gmail.com>
2023-05-04 21:43:41 +0530

    Code for deleting aws sagemaker notebook instance using boto3

---
+++ cloud-platform/aws/delete_sagemaker_notebook_instance.py
@@ -0,0 +1,29 @@
+## Importing Libraries
+import boto3
+
+## Constants
+AWS_ACCESS_KEY_ID = 'access_key_id'
+AWS_SECRET_ACCESS_KEY = 'secret_access_key'
+
+region_name = 'us-east-1'
+
+## Set up the Amazon SageMaker client
+sagemaker = boto3.client('sagemaker',
+                          region_name=region_name, 
+                          aws_access_key_id=AWS_ACCESS_KEY_ID,
+                          aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+## Define the name of the notebook instance to delete
+notebook_instance_name = 'notebook-instance-name'
+
+## Delete the notebook instance
+sagemaker.delete_notebook_instance(
+    NotebookInstanceName=notebook_instance_name
+)
+
+## Wait for the notebook instance to be deleted
+sagemaker.get_waiter('notebook_instance_deleted').wait(
+    NotebookInstanceName=notebook_instance_name
+)
+
+print(f"Notebook instance '{notebook_instance_name}' deleted successfully.")
